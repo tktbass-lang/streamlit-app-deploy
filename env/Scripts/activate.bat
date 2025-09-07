243d1e7095ce04724b66ecedb96d7cedf0602947
tktbass <tktbass@gmail.com>
2025-09-07 15:57:58 +0900

    first commit.

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tktba\program\streamlit-practice\env
+set VIRTUAL_ENV=C:\Users\tktba\program\streamlit-llm-app\env
 
 if not defined PROMPT set PROMPT=$P$G
 
